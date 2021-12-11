25d05286ae6ac54d59e803ebde66843cd5e90b5b
tsuchiya <tsuchiya@bengo4.com>
2021-12-11 12:14:21 +0900

    Disable exception and error handler of yii

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,4 +3,8 @@
 declare(strict_types=1);
 
 require __DIR__ . '/../vendor/autoload.php';
+
+define('YII_ENABLE_EXCEPTION_HANDLER', false);
+define('YII_ENABLE_ERROR_HANDLER', false);
+
 class_exists(YiiBase::class); // Avoid yii core interfaces already in use.
