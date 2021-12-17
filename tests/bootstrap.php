2e0902d608880829dffa6c674581892e4ac6e09f
tsuchiya <tsuchiya@bengo4.com>
2021-12-17 23:42:57 +0900

    Fix autoload

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,9 +2,8 @@
 
 declare(strict_types=1);
 
-require __DIR__ . '/../vendor/autoload.php';
-
 define('YII_ENABLE_EXCEPTION_HANDLER', false);
 define('YII_ENABLE_ERROR_HANDLER', false);
 
-class_exists(YiiBase::class); // Avoid yii core interfaces already in use.
+require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../vendor/yiisoft/yii/framework/yii.php';
